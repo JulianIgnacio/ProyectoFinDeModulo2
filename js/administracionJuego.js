915dca54091ea530dbd0791556e27ac899fa6b33
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-05-07 17:08:52 -0300

    se crean las variables del objecto juego del Modal Juego

--- js/administracionJuego.js
+++ js/administracionJuego.js
@@ -26,3 +26,14 @@ console.log(listaJuegos);
 //variables del objeto Juego del modal Juego
 let formularioAdminJuego = document.getElementById('formJuego');
 console.log(formularioAdminJuego);
+let codigo = document.getElementById('codigo'),
+  nombre = document.getElementById('nombre'),
+  precio = document.getElementById('precio'),
+  categoria = document.getElementById('categoria'),
+  stock = document.getElementById('stock'),
+  descripcion = document.getElementById('descripcion'),
+  imagen = document.getElementById('imagen'),
+  requisitosSistema = document.getElementById('requisitosSistema'),
+  desarrollador = document.getElementById('desarrollador');
+let modalFormJuego = new bootstrap.Modal(document.getElementById('modalJuego'));
+console.log(modalFormJuego);
