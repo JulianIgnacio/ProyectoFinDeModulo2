a4793192beae7938dc76d127757313056ae3754f
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-05-07 17:04:20 -0300

    se trae del localstorage la listaJuegos, y si no está creado un array vacio.

--- js/administracionJuego.js
+++ js/administracionJuego.js
@@ -1,1 +1,28 @@
-import Juego from './classJuego';
+import Juego from './classJuego.js';
+
+//variables globales
+let listaJuegos = JSON.parse(localStorage.getItem('listaJuegos')) || [];
+//saber si el array esta no vacio
+if (listaJuegos.length !== 0) {
+  //objecto Juego
+  listaJuegos = listaJuegos.map(
+    (juego) =>
+      new Juego(
+        juego.nombre,
+        juego.precio,
+        juego.categoria,
+        juego.stock,
+        juego.descripcion,
+        juego.imagen,
+        juego.requisitosSistema,
+        juego.desarrollador,
+        juego.reseniaVoto,
+        juego.reseniaDescripcion
+      )
+  );
+}
+console.log(listaJuegos);
+
+//variables del objeto Juego del modal Juego
+let formularioAdminJuego = document.getElementById('formJuego');
+console.log(formularioAdminJuego);
