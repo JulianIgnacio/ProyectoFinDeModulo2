0300f7a12c2116bcb6c360fdfc232e92a2fcbf43
JulianIgnacio <montenegrojulianig@gmail.com>
2023-05-15 17:03:54 -0300

    finalizando codigo logica-detalle.js

--- js/detalleJuego.js
+++ js/detalleJuego.js
@@ -5,7 +5,8 @@ let listaJuegos = JSON.parse(localStorage.getItem('listaJuegos')) || [];
 const juegoBuscado = listaJuegos.find((juego)=> juego.codigo === parametroCodigo.get('codigo'));
 
 let seccion1 = document.getElementById('#presentacion');
-seccion.innerHTML = `          <div class="row">
+seccion1.innerHTML = `
+<div class="row">
 <aside class="container-fluid h-25 w-75" id="sliderDetalle">
   <div id="carouselExample" class="carousel slide">
     <div class="carousel-inner ">
@@ -36,4 +37,34 @@ seccion.innerHTML = `          <div class="row">
     <p>${juegoBuscado.codigo}</p>
     <p>${juegoBuscado.desarrollador}</p>
   </aside>
+</div>`
+
+let seccion2 = document.getElementById('#descripcion');
+seccion2.innerHTML =`
+<h4>Descripcion</h4>
+<hr>
+<p>${juegoBuscado.descripcion}</p>`
+
+let seccion3 = document.getElementById('#requisitos');
+seccion3.innerHTML = `
+<h5>Requisitos del sistema</h5>
+<hr>
+<p>
+  <a class="btn btn-outline-info" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Windows</a>
+  <button class="btn btn-outline-info" type="button" data-bs-toggle="collapse" data-bs-target="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">macOS</button>
+</p>
+<div class="row" id="columnas">
+  <div class="col-6">
+    <div class="collapse multi-collapse" id="multiCollapseExample1">
+      <div class="card card-body">
+        <ul>
+          MÍNIMO:
+         <li>Procesador: ${juegoBuscado.requisitosProcesador}</li>
+         <li>Memoria: ${juegoBuscado.requisitosMemoriaram} </li>
+         <li>Gráficos: ${juegoBuscado.requisitosTarjetagrafica}</li>
+         <li>Almacenamiento: ${juegoBuscado.requisitosAlmacenamiento}</li>
+       </ul>
+      </div>
+    </div>
+  </div>
 </div>`
