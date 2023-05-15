a36dc82583a355378bf4c9a331ae7aa6db33e472
JulianIgnacio <montenegrojulianig@gmail.com>
2023-05-15 11:39:41 -0300

    detallando estructura del detalleJuego

--- js/detalleJuego.js
+++ js/detalleJuego.js
@@ -1,3 +1,5 @@
 const parametroCodigo = new URLSearchParams(window.location.search);
 
 let listaJuegos = JSON.parse(localStorage.getItem('listaJuegos')) || [];
+
+const peliculaBuscada = listaJuegos.find((juego)=> juego.codigo === parametroCodigo.get('codigo'));
